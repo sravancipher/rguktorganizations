71f7c783ab45c2d5ea4ab66ae1511cf5c9b76a80
sravancipher <83302891+sravancipher@users.noreply.github.com>
2022-11-17 16:47:27 +0530

    Add files via upload

---
+++ logindetails.php
@@ -0,0 +1,28 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="mysql";
+$database_name="ecrush_DB";
+$conn=mysqli_connect($servername,$username,$password,$database_name);
+if(!$conn)
+{
+    die("connection failed:".mysqli_connect_error());
+}
+if(isset($_POST['save']))
+{
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $number=$_POST['number'];
+    $sqlquery="INSERT INTO user_details(name,email,number) VALUES('$name','$email','$number')";
+    if(mysqli_query($conn,$sqlquery))
+    {
+        echo "New details recorded successfully";
+    }
+    else
+    {
+        echo "Error: ".$sql. "".mysqli_error($conn);
+    }
+    mysqli_close($conn);
+}
+?>
+
